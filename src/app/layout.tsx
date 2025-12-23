4e7f4c6b8b7edec53104ddc471921526ba6035e3
胡志勋 <huzx2006@fingard.com>
2025-12-23 18:40:17 +0800

    chore: add google adsense account meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,6 +60,7 @@ export default function RootLayout({
           name="google-site-verification"
           content="gjZiC-RxIRbV11DFbLLx5X7wESoAEDrQC0p5p0g8qU4"
         />
+        <meta name="google-adsense-account" content="ca-pub-4356459181693102" />
       </head>
       <body>
         {children}
